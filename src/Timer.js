0c321ac7818812244b49e897b438769851caee23
jbadan <jenna.badanowski@gmail.com>
2017-10-14 10:26:17 -0700

    bid button functionality

--- src/Timer.js
+++ src/Timer.js
@@ -6,7 +6,8 @@ class Timer extends Component {
   constructor(){
     super();
     this.state = {
-      seconds: 10
+      seconds: 10,
+      amount: 0
     };
     this.timer = 0;
     this.countDown = this.countDown.bind(this);
@@ -23,12 +24,26 @@ class Timer extends Component {
       clearInterval(this.timer);
     }
   }
+  bid(){
+    let newAmount = this.state.amount + 1;
+    this.setState({
+      amount: newAmount,
+      seconds: 10
+    })
+  }
   render() {
+    let userBidder = null;
+    if(this.state.amount === 0){
+      userBidder = <div>Nobody has bid yet!</div>
+    }else{
+      userBidder = <div>Farge bid!</div>
+    }
     return (
       <div className="App">
         <div> {this.state.seconds} </div>
-        <div> Amount </div>
-        <button>Bid!</button>
+        <div> ${this.state.amount} </div>
+        <div> {userBidder}</div>
+        <button disabled={this.state.seconds === 0} onClick={(e) => this.bid(e)}>Bid Now!</button>
       </div>
     );
   }
