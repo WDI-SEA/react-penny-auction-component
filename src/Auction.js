cf3d56c33c8280ea8f4a2672f2d1be94fed47163
justecii <justecii@gmail.com>
2017-10-15 15:35:29 -0700

    needs some style now

--- src/Auction.js
+++ src/Auction.js
@@ -6,44 +6,94 @@ import Countdown from './Countdown'
 class Auction extends Component {
     constructor(props) {
         super(props)
-    var sale = {
-        name: '$10 Shell Gift Card',
-        img: '/img/$10 Shell Gift Card.jpg',
-        price: 0,
+//     var sale = {
+//         name: '$10 Shell Gift Card',
+//         img: '/img/$10 Shell Gift Card.jpg',
+//         price: 0,
         
-    }   
-    this.state = {
-        item: sale.name,
-        image: sale.img,
-        price: (sale.price),
-        intro: 10
-    }
-    //bind this for function
-    this.addBid = this.addBid.bind(this);
-    this.resetTimer = this.resetTimer(this);
-}
+//     }   
+//     this.state = {
+//         item: sale.name,
+//         image: sale.img,
+//         price: (sale.price),
+//         intro: 10
+//     }
+//     //bind this for function
+//     this.addBid = this.addBid.bind(this);
+//     this.resetTimer = this.resetTimer(this);
+// }
 
 
-    addBid(e) {
-        e.preventDefault();
-        this.setState({
-            price: parseInt(this.state.price)+ 1,
-        });
-    }
-    resetTimer(e) {
-        this.setState({
-            time: 10
-        });
+//     addBid(e) {
+//         e.preventDefault();
+//         this.setState({
+//             price: parseInt(this.state.price)+ 1,
+//         });
+//     }
+//     resetTimer(e) {
+//         this.setState({
+//             time: 10
+//         });
+    this.items = [
+        {
+            title: "$10 Shell Gift Card",
+            src: "/img/$10 Shell Gift Card.jpg"
+        },
+        {
+            title: "$50 Walmart Gift Card",
+            src: "/img/$50 Walmart Gift Card.jpg"
+        },
+        {
+            title: "15 Voucher Bids",
+            src: "/img/15 Voucher Bids.jpg"
+        },
+        {
+            title: "250 Voucher Bids",
+            src: "/img/250 Voucher Bids.jpg"
+        },
+        {
+            title: "50 Voucher Bids",
+            src: "/img/50 Voucher Bids.jpg"
+        },
+        {
+            title: "Canon Wireless Printer",
+            src: "/img/Canon Pixma MG Series Wireless Printer.jpg"
+        },
+        {
+            title: "Cuisinart Bread Maker",
+            src: "/img/Cuisinart Convection Bread Maker.jpg"
+        },
+        {
+            title: "'Hide a Key' Sprinkler Head",
+            src: '/img/Discrete "Hide a Key" Sprinkler Head.jpg'
+        },
+        {
+            title: "Ultra-Soft Sheet Set",
+            src: "/img/Ultra-Soft 1800 Series Sheet Set.jpg"
+        },
+        {
+            title: "iPad Pro 9.7 32GB WifFi ",
+            src: '/img/iPad Pro 9.7" 32GB WifFi.jpg'
+        }
+        ]
+        this.state = {
+        itemArray: this.items
+        }
     }
     
     render() {
+        var mappedItems = this.state.itemArray.map(function(data, id){
+            return ([
+                <div className="saleItem"key={id}>
+                    <p key={id}>{data.title}</p>
+                    <img src={data.src} alt="Great Item"/>
+                    <Countdown />
+                </div>
+            ])
+        })
         return (
             <div>
-                <img src={this.state.image} alt="deal"/>
-                <Countdown start={this.state.intro} ms="1000"/>
-                <p>{this.state.item}</p>
-                <p>Current Price: $ {(this.state.price/100).toFixed(2)}</p>
-                <button className="" onClick={this.addBid} >{this.state.intro >0 ? this.state.item : "cabbage"}</button>
+                {mappedItems}
             </div>
         )
     }
--- src/Countdown.js
+++ src/Countdown.js
@@ -4,25 +4,56 @@ class Countdown extends Component {
     constructor(prop) {
         super(prop)
         this.state = {
-            ticks: parseInt(this.props.start)
+            ticks: 10,
+            price: 0
+        }
+        this.timer = 0;
+        this.tick = this.tick.bind(this);
+        if(this.timer === 0) {
+            this.timer = setInterval(this.tick, 1000)
         }
     }
-    componentDidMount(){
-        this.timerId = setInterval(() => this.tick(), this.props.ms);
-    }
-    componentWillUnmount(){
-        clearInterval(this.timerId)
-    }
+    // componentDidMount(){
+    //     this.timerId = setInterval(() => this.tick(), this.props.ms);
+    // }
+    // componentWillUnmount(){
+    //     clearInterval(this.timerId)
+    // }
     tick(){
-        this.setState((state, props) => ({
-            ticks: state.ticks > 0 ? state.ticks - 1 : console.log("finished") 
-        }));
+        let ticks = this.state.ticks -1;
+        this.setState({
+            ticks: ticks
+        });
+        if (ticks === 0) {
+            clearInterval(this.timer);
+        }
+    }
+    bid(){
+        let newPrice = this.state.price + 1;
+        this.setState({
+            price: newPrice,
+            ticks: 10
+        })
     }
     
     render(){
+        let userBidder = null;
+        if (this.state.price === 0) {
+            userBidder = <div>Nobody has Bid yet!</div>
+        } else {
+            userBidder = <div>Viper!</div>
+        }
+        if(this.state.ticks === 0 && this.state.price === 0){
+            userBidder = <div> Nobody bid on this item </div>
+        }else if(this.state.ticks === 0 && this.state.price >= 1){
+            userBidder= <div className="animationText"> Viper won! </div>
+        }
         return(
-            <div>
+            <div className="bidder">
                 <h1>{this.state.ticks}</h1>
+                <h4>Current Price: ${(this.state.price/100).toFixed(2)}</h4>
+                {userBidder}
+                <button disable={this.state.ticks === 0} onClick={(e) => this.bid(e)}>Bid Now</button>
             </div>
         )
     }
