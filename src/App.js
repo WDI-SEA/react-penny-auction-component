9a3c365b3f3e3299fc180b7985cfa4c51b5640d4
unclebconnor <bc.boxomail@gmail.com>
2017-10-15 23:13:39 -0700

    looks dope.  button works to update bids and bidder.  only one user.  timer counts down but can't figure out how to reset

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import Items from './Items.js';
+// import Timer from './Timer.js';
 import './App.css';
 
 class App extends Component {
+
   render() {
+    
     return (
       <div className="App">
-        <div className="buffer"></div>
-        <div className="itemRow"><Items /></div>
+        <div id="pageWrapper">
+          <div className="buffer"></div>
+          <div className="itemRow"><Items /></div>
+        </div>
       </div>
     );
   }
--- src/items.js
+++ src/items.js
@@ -1,245 +1,262 @@
 import React, { Component } from 'react';
+import Timer from './Timer.js';
 import './App.css';
 
 class Items extends Component {
   constructor(props) {
     super(props);
     this.state = ({
-      butt: "butt",
       itemList: [
-        {
-          item: '2nd Potion',
-          link: '2nd-potion.png',
-          secondsLeft: 10,
-          currentPrice: 0,
-          currentHighBidder: ''
-        },
-        {
-          item: 'Arrow',
-          link: 'arrow.png',
-          secondsLeft: 10,
-          currentPrice: 0,
-          currentHighBidder: ''
-        },
+        // {
+        //   item: '2nd Potion',
+        //   link: '2nd-potion.png',
+        //   secondsLeft: 10,
+        //   currentPrice: 0,
+        //   currentHighBidder: ''
+        // },
+        // {
+        //   item: 'Arrow',
+        //   link: 'arrow.png',
+        //   secondsLeft: 10,
+        //   currentPrice: 0,
+        //   currentHighBidder: ''
+        // },
         {
           item: 'Blue Candle',
           link: 'blue-candle.png',
-          secondsLeft: 10,
+          secondsLeft: <Timer />,
           currentPrice: 0,
           currentHighBidder: ''
         },
-        {
-          item: 'Blue Ring',
-          link: 'blue-ring.png',
-          secondsLeft: 10,
-          currentPrice: 0,
-          currentHighBidder: ''
-        },
-        {
-          item: 'Bomb',
-          link: 'bomb.png',
-          secondsLeft: 10,
-          currentPrice: 0,
-          currentHighBidder: ''
-        },
+        // {
+        //   item: 'Blue Ring',
+        //   link: 'blue-ring.png',
+        //   secondsLeft: 10,
+        //   currentPrice: 0,
+        //   currentHighBidder: ''
+        // },
+        // {
+        //   item: 'Bomb',
+        //   link: 'bomb.png',
+        //   secondsLeft: 10,
+        //   currentPrice: 0,
+        //   currentHighBidder: ''
+        // },
         {
           item: 'Book of Magic',
           link: 'book-of-magic.png',
-          secondsLeft: 10,
+          secondsLeft: <Timer />,
           currentPrice: 0,
           currentHighBidder: ''
         },
-        {
-          item: 'Boomerang',
-          link: 'boomerang.png',
-          secondsLeft: 10,
-          currentPrice: 0,
-          currentHighBidder: ''
-        },
-        {
-          item: 'Bow',
-          link: 'bow.png',
-          secondsLeft: 10,
-          currentPrice: 0,
-          currentHighBidder: ''
-        },
-        {
-          item: 'Compass',
-          link: 'compass.png',
-          secondsLeft: 10,
-          currentPrice: 0,
-          currentHighBidder: ''
-        },
+        // {
+        //   item: 'Boomerang',
+        //   link: 'boomerang.png',
+        //   secondsLeft: 10,
+        //   currentPrice: 0,
+        //   currentHighBidder: ''
+        // },
+        // {
+        //   item: 'Bow',
+        //   link: 'bow.png',
+        //   secondsLeft: 10,
+        //   currentPrice: 0,
+        //   currentHighBidder: ''
+        // },
+        // {
+        //   item: 'Compass',
+        //   link: 'compass.png',
+        //   secondsLeft: 10,
+        //   currentPrice: 0,
+        //   currentHighBidder: ''
+        // },
         {
           item: 'Food',
           link: 'food.png',
-          secondsLeft: 10,
+          secondsLeft: <Timer />,
           currentPrice: 0,
           currentHighBidder: ''
         },
-        {
-          item: 'Key',
-          link: 'key.png',
-          secondsLeft: 10,
-          currentPrice: 0,
-          currentHighBidder: ''
-        },
-        {
-          item: 'Letter',
-          link: 'letter.png',
-          secondsLeft: 10,
-          currentPrice: 0,
-          currentHighBidder: ''
-        },
-        {
-          item: 'Life-Potion',
-          link: 'life-potion.png',
-          secondsLeft: 10,
-          currentPrice: 0,
-          currentHighBidder: ''
-        },
-        {
-          item: 'Magical Boomerang',
-          link: 'magical-boomerang.png',
-          secondsLeft: 10,
-          currentPrice: 0,
-          currentHighBidder: ''
-        },
-        {
-          item: 'Magical Key',
-          link: 'magical-key.png',
-          secondsLeft: 10,
-          currentPrice: 0,
-          currentHighBidder: ''
-        },
-        {
-          item: 'Magical-Rod',
-          link: 'magical-rod.png',
-          secondsLeft: 10,
-          currentPrice: 0,
-          currentHighBidder: ''
-        },
-        {
-          item: 'Magical Shield',
-          link: 'magical-shield.png',
-          secondsLeft: 10,
-          currentPrice: 0,
-          currentHighBidder: ''
-        },
-        {
-          item: 'Magical Sword',
-          link: 'magical-sword.png',
-          secondsLeft: 10,
-          currentPrice: 0,
-          currentHighBidder: ''
-        },
-        {
-          item: 'map',
-          link: 'map.png',
-          secondsLeft: 10,
-          currentPrice: 0,
-          currentHighBidder: ''
-        },
-        {
-          item: 'Power Bracelet',
-          link: 'power-bracelet.png',
-          secondsLeft: 10,
-          currentPrice: 0,
-          currentHighBidder: ''
-        },
-        {
-          item: 'Raft',
-          link: 'raft.png',
-          secondsLeft: 10,
-          currentPrice: 0,
-          currentHighBidder: ''
-        },
-        {
-          item: 'Recorder',
-          link: 'recorder.png',
-          secondsLeft: 10,
-          currentPrice: 0,
-          currentHighBidder: ''
-        },
-        {
-          item: 'Red-Candle',
-          link: 'red-candle.png',
-          secondsLeft: 10,
-          currentPrice: 0,
-          currentHighBidder: ''
-        },
-        {
-          item: 'Red-Ring',
-          link: 'red-ring.png',
-          secondsLeft: 10,
-          currentPrice: 0,
-          currentHighBidder: ''
-        },
-        {
-          item: 'Rupee',
-          link: 'rupee.png',
-          secondsLeft: 10,
-          currentPrice: 0,
-          currentHighBidder: ''
-        },
-        {
-          item: 'Silver-Arrow',
-          link: 'silver-arrow.png',
-          secondsLeft: 10,
-          currentPrice: 0,
-          currentHighBidder: ''
-        },
-        {
-          item: 'Stepladder',
-          link: 'stepladder.png',
-          secondsLeft: 10,
-          currentPrice: 0,
-          currentHighBidder: ''
-        },
-        {
-          item: 'Sword',
-          link: 'sword.png',
-          secondsLeft: 10,
-          currentPrice: 0,
-          currentHighBidder: ''
-        },
-        {
-          item: 'White Sword',
-          link: 'white-sword.png',
-          secondsLeft: 10,
-          currentPrice: 0,
-          currentHighBidder: ''
-        },
-        {
-          item: 'Wooden Shield',
-          link: 'wooden-shield.png',
-          secondsLeft: 10,
-          currentPrice: 0,
-          currentHighBidder: ''
-        }
+        // {
+        //   item: 'Key',
+        //   link: 'key.png',
+        //   secondsLeft: 10,
+        //   currentPrice: 0,
+        //   currentHighBidder: ''
+        // },
+        // {
+        //   item: 'Letter',
+        //   link: 'letter.png',
+        //   secondsLeft: 10,
+        //   currentPrice: 0,
+        //   currentHighBidder: ''
+        // },
+        // {
+        //   item: 'Life-Potion',
+        //   link: 'life-potion.png',
+        //   secondsLeft: 10,
+        //   currentPrice: 0,
+        //   currentHighBidder: ''
+        // },
+        // {
+        //   item: 'Magical Boomerang',
+        //   link: 'magical-boomerang.png',
+        //   secondsLeft: 10,
+        //   currentPrice: 0,
+        //   currentHighBidder: ''
+        // },
+        // {
+        //   item: 'Magical Key',
+        //   link: 'magical-key.png',
+        //   secondsLeft: 10,
+        //   currentPrice: 0,
+        //   currentHighBidder: ''
+        // },
+        // {
+        //   item: 'Magical-Rod',
+        //   link: 'magical-rod.png',
+        //   secondsLeft: 10,
+        //   currentPrice: 0,
+        //   currentHighBidder: ''
+        // },
+        // {
+        //   item: 'Magical Shield',
+        //   link: 'magical-shield.png',
+        //   secondsLeft: 10,
+        //   currentPrice: 0,
+        //   currentHighBidder: ''
+        // },
+        // {
+        //   item: 'Magical Sword',
+        //   link: 'magical-sword.png',
+        //   secondsLeft: 10,
+        //   currentPrice: 0,
+        //   currentHighBidder: ''
+        // },
+        // {
+        //   item: 'map',
+        //   link: 'map.png',
+        //   secondsLeft: 10,
+        //   currentPrice: 0,
+        //   currentHighBidder: ''
+        // },
+        // {
+        //   item: 'Power Bracelet',
+        //   link: 'power-bracelet.png',
+        //   secondsLeft: 10,
+        //   currentPrice: 0,
+        //   currentHighBidder: ''
+        // },
+        // {
+        //   item: 'Raft',
+        //   link: 'raft.png',
+        //   secondsLeft: 10,
+        //   currentPrice: 0,
+        //   currentHighBidder: ''
+        // },
+        // {
+        //   item: 'Recorder',
+        //   link: 'recorder.png',
+        //   secondsLeft: 10,
+        //   currentPrice: 0,
+        //   currentHighBidder: ''
+        // },
+        // {
+        //   item: 'Red-Candle',
+        //   link: 'red-candle.png',
+        //   secondsLeft: 10,
+        //   currentPrice: 0,
+        //   currentHighBidder: ''
+        // },
+        // {
+        //   item: 'Red-Ring',
+        //   link: 'red-ring.png',
+        //   secondsLeft: 10,
+        //   currentPrice: 0,
+        //   currentHighBidder: ''
+        // },
+        // {
+        //   item: 'Rupee',
+        //   link: 'rupee.png',
+        //   secondsLeft: 10,
+        //   currentPrice: 0,
+        //   currentHighBidder: ''
+        // },
+        // {
+        //   item: 'Silver-Arrow',
+        //   link: 'silver-arrow.png',
+        //   secondsLeft: 10,
+        //   currentPrice: 0,
+        //   currentHighBidder: ''
+        // },
+        // {
+        //   item: 'Stepladder',
+        //   link: 'stepladder.png',
+        //   secondsLeft: 10,
+        //   currentPrice: 0,
+        //   currentHighBidder: ''
+        // },
+        // {
+        //   item: 'Sword',
+        //   link: 'sword.png',
+        //   secondsLeft: 10,
+        //   currentPrice: 0,
+        //   currentHighBidder: ''
+        // },
+        // {
+        //   item: 'White Sword',
+        //   link: 'white-sword.png',
+        //   secondsLeft: 10,
+        //   currentPrice: 0,
+        //   currentHighBidder: ''
+        // },
+        // {
+        //   item: 'Wooden Shield',
+        //   link: 'wooden-shield.png',
+        //   secondsLeft: 10,
+        //   currentPrice: 0,
+        //   currentHighBidder: ''
+        // }
       ]
     })
   }
 
+  componentDidMount() {
+
+  }
+
+  handleClick(e,index){
+    e.preventDefault();
+    let newItemList = this.state.itemList;
+    newItemList[index].currentPrice+=1;
+    newItemList[index].currentHighBidder = 'Link';
+    this.setState({
+      itemList: newItemList
+    })
+  }
+
   render() {
-    console.log(this.state);
-    return (
+    
+      return (
       <div className="itemSection">
-
-
         {
           this.state.itemList.map((item,index) => 
+
             <div key={index} className="componentWrapper">
-              <h2>{item.item}</h2>
+              <h3>{item.item}</h3>
               <img src={`img/${item.link}`} alt="" />
-              <div class="itemInfo">
+              <div className="itemInfo">
                 <p>
                   High Bid: {item.currentPrice} x {<img src="img/rupee.png" alt=""/>}
                 </p>
               </div>
-              <div class="itemInfo"><p>High Bidder: {item.currentHighBidder}</p></div>
-              <div class="itemInfo"><p>Time Left: {item.secondsLeft}</p></div>
-              <button>{<img src="img/rupee.png" alt=""/>} <div>+ 1</div></button>
+              <div className="itemInfo"><p>Bidder: {item.currentHighBidder}</p></div>
+              <div className="itemInfo">
+                <p>Timer: <Timer secondsLeft={item.secondsLeft}/></p>
+              </div>
+              <button onClick={(e) => this.handleClick(e,index)}>
+                {<img src="img/rupee.png" alt=""/>} <div>+ 1</div>
+              </button>
             </div>
           )
         }
