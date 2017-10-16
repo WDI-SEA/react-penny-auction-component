bc4771b044ad6568fa057ea2c5ca175a2177b4f1
jbadan <jenna.badanowski@gmail.com>
2017-10-16 09:21:27 -0700

    fixed 31202$

--- src/Timer.js
+++ src/Timer.js
@@ -25,7 +25,8 @@ class Timer extends Component {
     }
   }
   bid(){
-    let newAmount = this.state.amount + 1;
+    let newAmount = this.state.amount + .01;
+    newAmount = Math.round( newAmount * 100 + Number.EPSILON ) / 100
     this.setState({
       amount: newAmount,
       seconds: 10
@@ -40,7 +41,7 @@ class Timer extends Component {
   }
   bidColor(){
     let classname = ""
-    if(this.state.amount >= 1){
+    if(this.state.amount >= .01){
       classname+= "green"
     }
     return classname;
@@ -54,12 +55,18 @@ class Timer extends Component {
     }
     if(this.state.seconds === 0 && this.state.amount === 0){
       userBidder = <div> Nobody bid on this item </div>
-    }else if(this.state.seconds === 0 && this.state.amount >= 1){
+    }else if(this.state.seconds === 0 && this.state.amount >= .01){
       userBidder= <div className="animationText"> Farge won! </div>
+    }
+    let fullTimer = null;
+    if(this.state.seconds === 10){
+      fullTimer = <div>00:00:{this.state.seconds} </div>
+    }else{
+      fullTimer = <div>00:00:0{this.state.seconds} </div>
     }
     return (
       <div className="Bidder">
-        <div> 00:00:{this.state.seconds} </div>
+        <div> {fullTimer} </div>
         <div className={this.bidColor()}> ${this.state.amount} </div>
         <div> {userBidder}</div>
         <button className={this.animationClass()} disabled={this.state.seconds === 0} onClick={(e) => this.bid(e)}>Bid Now!</button>
