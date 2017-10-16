b71d83a7685fa19478e14242fbf6f6d16f525430
regbake <regbake@gmail.com>
2017-10-16 08:19:12 -0700

    current state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,110 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Items from "./Items";
+import List from "./List";
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [
+        {
+          title: "Shell Gift Card",
+          timeStart: Date.now(),
+          timeLeft: "00:00",
+          currentPrice: "$",
+          img: "/img/$10 Shell Gift Card.jpg"
+        },
+        {
+          title: "Walmart Gift Card",
+          timeStart: Date.now(),
+          timeLeft: "00:00",
+          currentPrice: "$",
+          img: "/img/$50 Walmart Gift Card.jpg"
+        },
+        {
+          title: "15 Voucher Bids",
+          timeStart: Date.now(),
+          timeLeft: "00:00",
+          currentPrice: "$0",
+          img: "/img/15 Voucher Bids.jpg"
+        },
+        {
+          title: "250 Voucher Bids",
+          timeStart: Date.now(),
+          timeLeft: "00:00",
+          currentPrice: "$0",
+          img: "/img/250 Voucher Bids.jpg"
+        },
+        {
+          title: "50 Voucher Bids",
+          timeStart: Date.now(),
+          timeLeft: "00:00",
+          currentPrice: "$0",
+          img: "/img/50 Voucher Bids.jpg"
+        },
+        {
+          title: "Canon Pixma MG Series Wireless Printer",
+          timeStart: Date.now(),
+          timeLeft: "00:00",
+          currentPrice: "$0",
+          img: "/img/Canon Pixma MG Series Wireless Printer.jpg"
+        },
+        {
+          title: "Cuisinart Convection Bread Maker",
+          timeStart: Date.now(),
+          timeLeft: "00:00",
+          currentPrice: "$0",
+          img: "/img/Cuisinart Convection Bread Maker.jpg"
+        },
+        {
+          title: 'Discrete "Hide a Key" Sprinkler Head',
+          timeStart: Date.now(),
+          timeLeft: "00:00",
+          currentPrice: "$0",
+          img: '/img/Discrete "Hide a Key" Sprinkler Head.jpg'
+        },
+        {
+          title: "Ultra-Soft 1800 Series Sheet Set",
+          timeStart: Date.now(),
+          timeLeft: "00:00",
+          currentPrice: "$0",
+          img: "/img/Ultra-Soft 1800 Series Sheet Set.jpg"
+        },
+        {
+          title: 'iPad Pro 9.7" 32GB WiFi',
+          timeStart: Date.now(),
+          timeLeft: "00:00",
+          currentPrice: "$0",
+          img: '/img/iPad Pro 9.7" 32GB WiFi.jpg'
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <div>
-        <Items />
+        <List items={this.state.items}
+              addToBid={(itemIndex) => this.addToBid(itemIndex)}
+              countdown={(itemIndex) => this.countdown(itemIndex)}
+              addToTimeLeft={(itemIndex) => this.addToTimeLeft(itemIndex)} />
       </div>
     );
+  }
+
+  addToBid(itemIndex) {
+    let items = this.state.items;
+    items[itemIndex].currnetBid += 0.01;
+    this.setState({items: items});
+  }
+
+  countdown(itemIndex) {
+    let items = this.state.items;
+    items[itemIndex].timeLeft -= 1;
+    this.setState({items: items});
   }
 }
 
--- src/Items.js
+++ src/Items.js
@@ -1,105 +1,28 @@
 import React, { Component } from 'react';
-//import the "Template"
-import Template from "./Template"
 
 class Items extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      items: [
-        {
-          title: "Shell Gift Card",
-          timeLeft: "00:00",
-          currentPrice: "$10",
-          user: "Reg",
-          img: "/img/$10 Shell Gift Card.jpg"
-        },
-        {
-          title: "Walmart Gift Card",
-          timeLeft: "00:00",
-          currentPrice: "$50",
-          user: "Reg",
-          img: "/img/$50 Walmart Gift Card.jpg"
-        },
-        {
-          title: "15 Voucher Bids",
-          timeLeft: "00:00",
-          currentPrice: "$0",
-          user: "Reg",
-          img: "/img/15 Voucher Bids.jpg"
-        },
-        {
-          title: "250 Voucher Bids",
-          timeLeft: "00:00",
-          currentPrice: "$0",
-          user: "Reg",
-          img: "/img/250 Voucher Bids.jpg"
-        },
-        {
-          title: "50 Voucher Bids",
-          timeLeft: "00:00",
-          currentPrice: "$0",
-          user: "Reg",
-          img: "/img/50 Voucher Bids.jpg"
-        },
-        {
-          title: "Canon Pixma MG Series Wireless Printer",
-          timeLeft: "00:00",
-          currentPrice: "$0",
-          user: "Reg",
-          img: "/img/Canon Pixma MG Series Wireless Printer.jpg"
-        },
-        {
-          title: "Cuisinart Convection Bread Maker",
-          timeLeft: "00:00",
-          currentPrice: "$0",
-          user: "Reg",
-          img: "/img/Cuisinart Convection Bread Maker.jpg"
-        },
-        {
-          title: 'Discrete "Hide a Key" Sprinkler Head',
-          timeLeft: "00:00",
-          currentPrice: "$0",
-          user: "Reg",
-          img: '/img/Discrete "Hide a Key" Sprinkler Head.jpg'
-        },
-        {
-          title: "Ultra-Soft 1800 Series Sheet Set",
-          timeLeft: "00:00",
-          currentPrice: "$0",
-          user: "Reg",
-          img: "/img/Ultra-Soft 1800 Series Sheet Set.jpg"
-        },
-        {
-          title: 'iPad Pro 9.7" 32GB WiFi',
-          timeLeft: "00:00",
-          currentPrice: "$0",
-          user: "Reg",
-          img: '/img/iPad Pro 9.7" 32GB WiFi.jpg'
-        }
-      ]
-    }
-  }
-
   render() {
+    let dateToFormat = "1976-04-19T12:59-0500";
+    let bid = parseFloat(Math.round(this.props.currentBid*100)/100).toFixed(2);
+
     return (
-      <div>
-        {this.getItems}
+      <div className="holder">
+        <p> {this.props.name} </p>
+        <img src={this.props.image} alt={this.props.name} />
+        <p> {this.props.timeLeft} </p>
+        <p> ${bid} </p>
+        <button onClick={() => {this.addPenny()}}>Bid Now! </button>
       </div>
     );
   }
 
-  getItems() {
-    return this.state.items.map(item => {
-      return (< Template
-        title = { item.title }
-        timeLeft = {item.timeLeft}
-        currentPrice = {item.currentPrice}
-        user = {item.user}
-        img = {item.img}
-        />);
-    });
+  addPenny() {
+    this.props.addBid(this.props.index);
+    this.props.addToTimeLeft(this.props.index);
+  }
+
+  counddownTimer() {
+    this.props.countdown(this.props.index);
   }
 }
 
